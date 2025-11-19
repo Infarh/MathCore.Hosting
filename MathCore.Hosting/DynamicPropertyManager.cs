e68278848561564b5950f99007a9d0b61f151941
Infarh <shmachilin@gmail.com>
2025-11-19 20:09:32 +0300

    v0.0.6 - Обновление пакетов (до ,NET10)

--- MathCore.Hosting/DynamicPropertyManager.cs
+++ MathCore.Hosting/DynamicPropertyManager.cs
@@ -1,32 +1,30 @@
-﻿using System;
-using System.ComponentModel;
+﻿//using System;
+//using System.ComponentModel;
 
-namespace MathCore.Hosting;
+//namespace MathCore.Hosting;
 
-public class DynamicPropertyManager<T> : IDisposable
-{
-    private DynamicTypeDescriptionProvider _Provider;
-    private class DynamicTypeDescriptionProvider : TypeDescriptionProvider
-    {
+//public class DynamicPropertyManager<T> : IDisposable
+//{
+//    private readonly DynamicTypeDescriptionProvider _Provider;
 
-    }
+//    private class DynamicTypeDescriptionProvider : TypeDescriptionProvider;
 
-    public DynamicPropertiesCollection<T> Properties { get; } = new();
+//    public DynamicPropertiesCollection<T> Properties { get; } = new();
 
-    public DynamicPropertyManager() => TypeDescriptor.AddProvider(_Provider = new DynamicTypeDescriptionProvider(), typeof(T));
+//    public DynamicPropertyManager() => TypeDescriptor.AddProvider(_Provider = new(), typeof(T));
 
-    public void Dispose() => TypeDescriptor.RemoveProvider(_Provider, typeof(T));
-}
+//    public void Dispose() => TypeDescriptor.RemoveProvider(_Provider, typeof(T));
+//}
 
-public class DynamicPropertiesCollection<T>
-{
-    private class DynamicPropertyDescriptor<TProperty>
-    {
+//public class DynamicPropertiesCollection<T>
+//{
+//    private class DynamicPropertyDescriptor<TProperty>
+//    {
 
-    }
+//    }
 
-    public void Add<TProperty>(string Name, Func<TProperty> Getter, Action<TProperty>? Setter = null, Attribute[]? Attributes = null)
-    {
+//    public void Add<TProperty>(string Name, Func<TProperty> Getter, Action<TProperty>? Setter = null, Attribute[]? Attributes = null)
+//    {
 
-    }
-}
+//    }
+//}
--- MathCore.Hosting/ServiceLocator.cs
+++ MathCore.Hosting/ServiceLocator.cs
@@ -4,8 +4,12 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace MathCore.Hosting;
 
+/// <summary>Базовый локатор сервисов с доступом к зависимостям через dynamic</summary>
 public abstract class ServiceLocator : DynamicObject
 {
+    /// <summary>Заполняет внутренний кэш доступных сервисов на основе текущих регистраций</summary>
+    /// <param name="services">Коллекция сервисов DI</param>
+    /// <remarks>При совпадении имён типов берётся первая регистрация</remarks>
     public static void ConfigureServices(IServiceCollection services)
     {
         var result = new Dictionary<string, (Type? Type, Func<IServiceProvider, object>? Factory)>();
@@ -19,12 +23,19 @@ public abstract class ServiceLocator : DynamicObject
         __Services = result;
     }
 
+    /// <summary>Кэш соответствий имён сервисов их типам или фабрикам</summary>
     private static Dictionary<string, (Type? Type, Func<IServiceProvider, object>? Factory)> __Services = new();
 
+    /// <summary>Провайдер сервисов, используемый локатором для разрешения зависимостей</summary>
     protected abstract IServiceProvider Services { get; }
 
-    /// <inheritdoc />
-    public override bool TryGetMember(GetMemberBinder binder, out object result)
+    /// <summary>Пытается получить сервис по имени динамического члена</summary>
+    /// <param name="binder">Объект привязки динамического члена</param>
+    /// <param name="result">Найденный экземпляр сервиса или null</param>
+    /// <returns>Истина, если сервис получен успешно</returns>
+    /// <exception cref="InvalidOperationException">Если тип найден, но экземпляр не может быть разрешён контейнером</exception>
+    /// <remarks>Сначала используется фабрика реализации, затем тип и провайдер сервисов</remarks>
+    public override bool TryGetMember(GetMemberBinder binder, out object? result)
     {
         if (base.TryGetMember(binder, out result))
             return true;
@@ -42,5 +53,7 @@ public abstract class ServiceLocator : DynamicObject
         return true;
     }
 
+    /// <summary>Возвращает имена доступных динамических членов</summary>
+    /// <returns>Перечень имён сервисов, доступных через локатор</returns>
     public override IEnumerable<string> GetDynamicMemberNames() => __Services.Keys;
 }
